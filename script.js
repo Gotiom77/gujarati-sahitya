5c707e1cef754aad668524a1f8d09d994b0760ca
goti om <omgoti30@example.com>
2025-09-26 17:21:48 +0530

    new project

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Button click alert example
 document.querySelectorAll('.btn').forEach(button => {
   button.addEventListener('click', () => {
     alert('Thank you for clicking the "' + button.textContent + '" button!');
